5c4252dfb3f46ee167320a3e4b8faa2f7d87231d
Jan Korte <j.korte@me.com>
2015-09-11 22:22:24 +0200

    crawler works

--- crawl.py
+++ crawl.py
@@ -5,12 +5,34 @@ CATEGORY_LIST = [
     ('http://www.ecocion-shop.de/Obst-frisch/Beeren%2C-Trauben_21327.html', 'Beeren und Trauben'),
     ('http://www.ecocion-shop.de/Obst-frisch/Kernobst_21328.html', 'Kernobst'),
     ('http://www.ecocion-shop.de/Obst-frisch/Steinobst_21329.html', 'Steinobst'),
+    ('http://www.ecocion-shop.de/Obst-frisch/Suedfruechte_21330.html', 'Südfrüchte'),
+    ('http://www.ecocion-shop.de/Obst-frisch/Zitrusfruechte_21331.html', 'Zitrusfrüchte'),
+    ('http://www.ecocion-shop.de/Obst-frisch/Nuesse_21468.html', 'Nüsse'),
+    ('http://www.ecocion-shop.de/Gemuese-frisch/Blattgemuese-Stangengemuese_21308.html', 'Blattgemüse und Stangengemüse'),
+    ('http://www.ecocion-shop.de/Fruchtgemuese_21310.html', 'Fruchtgemüse'),
+    ('http://www.ecocion-shop.de/Gemuese-frisch/Kohlgemuese_21311.html', 'Kohlgemüse'),
+    ('http://www.ecocion-shop.de/Gemuese-frisch/Kraeuter-%26-Spargel_21312.html', 'Kräuter & Spargel'),
+    ('http://www.ecocion-shop.de/Gemuese-frisch/Lauch--und-Zwiebelgemuese_21313.html', 'Lauch- und Zwiebelgemüse'),
+    ('http://www.ecocion-shop.de/Gemuese-frisch/Pilze_21314.html', 'Pilze'),
+    ('http://www.ecocion-shop.de/Gemuese-frisch/Salat_21315.html', 'Salat'),
+    ('http://www.ecocion-shop.de/Gemuese-frisch/Kartoffeln_21316.html', 'Kartoffeln'),
+    ('http://www.ecocion-shop.de/Gemuese-frisch/Wurzel--und-Knollengemuese_21317.html', 'Wurzel- und Knollengemüse'),
 ]
+TROCKENWARE = 'Trockenware'
+ALL_ITEMS = [('http://www.ecocion-shop.de/web/main.php/shop/index/seite/21273?pper_page=10000&switchView=true', TROCKENWARE)]
+
+
+def write_file(filename, item_list):
+    with open(filename, 'w') as file:
+        for item in item_list:
+            file.write(item + '\n')
+
 
 if __name__ == '__main__':
-    item_list = ['first row has to be empty']
+    fresh_item_list = ['first row has to be empty']
+    dry_item_list = ['first row has to be empty']
 
-    for url, category in CATEGORY_LIST:
+    for url, category in CATEGORY_LIST + ALL_ITEMS:
         html_doc = requests.get(url).text
         soup = BeautifulSoup(html_doc, 'html.parser')
         # print(soup.prettify())
@@ -21,13 +43,18 @@ if __name__ == '__main__':
             pfand = item_div.find('input', attrs={'name': 'pfand'})['value']
             name = item_div.find('input', attrs={'name': 'name'})['value']
             einheit = item_div.find('span', class_='plEinheit')['title']  # find <span class='plEinheit' id='einheit_579440' title='kg'>kg</span>)
+            name = name[:59]  # cut too long names
             if einheit == '':
                 einheit_dropdown = item_div.find('select', class_='stk_best')
                 options = einheit_dropdown.findAll('option')  # find <option id="opt_660778" value="0"> Kg</option>
                 einheit = options[0].string  # get ' Kg'
-                name += ' ' + options[1].string.replace('Stk ', '')
+                append = ' ' + options[1].string.replace('Stk ', '')
+                if len(name + append) > 60:  # cut name if name is too long
+                    name = name[:(59 - len(append))]
+                name = name + append
+            if len(einheit) == 1:
+                einheit = einheit + '_'
             notiz = item_div.find('a')['href']
-
             row = ';{bestellnummer};{name};{notiz};;;{einheit};{preis};{mehrwertsteuer};{pfand};{gebindegroesse};{staffelmenge};{staffelpreis};{category}'
             # the foodsoft wants each row / item in this form, e.g. ;;Erdnussmus fein;;;;500 g;4,99;-0,17;0;1;;;Other
             mehrwertsteuer = '-17'
@@ -36,12 +63,15 @@ if __name__ == '__main__':
             staffelpreis = ''
             item = row.format_map(vars())
             # print(item)
-            if not any([bestellnummer in item for item in item_list]): # prevent doubles
-                item_list.append(item)
+            same_bestellnummer = any([bestellnummer in item for item in fresh_item_list])
+            same_name = any([name in item for item in dry_item_list])
+            if not same_bestellnummer and not same_name:
+                if category == TROCKENWARE:
+                    dry_item_list.append(item)
+                else:
+                    fresh_item_list.append(item)
 
-    item_list_without_bags = [item for item in item_list if 'Tüten' not in item]
+    fresh_item_list_without_bags = [item for item in fresh_item_list if 'Tüten' not in item]
 
-    with open('articles.csv', 'w') as file:
-        file.write(item + '\n')
-        for item in item_list_without_bags:
-            file.write(item + '\n')
+    write_file('fresh_articles.csv', fresh_item_list_without_bags)
+    write_file('dry_articles.csv', dry_item_list)
--- order.py
+++
@@ -1,8 +0,0 @@
-import webbrowser
-import re
-
-with open('input.txt', 'r') as file:
-    for row in file:
-        	new_row = re.sub(r' ', '', row)
-
-webbrowser.open(url)
